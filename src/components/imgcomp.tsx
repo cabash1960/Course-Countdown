27020840cd3645e5c5c01cfec252268564e58dcb
cabash1960 <blessingxin1960@gmail.com>
2026-01-13 12:41:23 +0100

    deployment : folder fixed

--- src/components/imgcomp.tsx
+++ src/components/imgcomp.tsx
@@ -1,15 +1,10 @@
-import a from "../assets/c.gif";
-import b from "../assets/d.gif";
-import c from "../assets/c.gif";
-import d from "../assets/d.gif";
-
 function Imgcomp() {
   return (
     <div className="grid grid-cols-2 md:grid-cols-4 w-full max-w-8xl ">
-      <img src={a} alt="a" />
-      <img src={b} alt="b" />
-      <img src={c} alt="c" />
-      <img src={d} alt="d" />
+      <img src="/c.GIF" alt="a" />
+      <img src="/d.GIF" alt="b" />
+      <img src="/c.GIF" alt="c" />
+      <img src="/d.GIF" alt="d" />
     </div>
   );
 }
