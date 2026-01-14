2aa2f727809ff89089712d0d8b8ff8281606502e
cabash1960 <blessingxin1960@gmail.com>
2026-01-14 03:55:43 +0100

    update: image class issue

--- src/components/imgcomp.tsx
+++ src/components/imgcomp.tsx
@@ -1,6 +1,6 @@
 function Imgcomp({ className }: { className?: string }) {
   return (
-    <div className="grid grid-cols-2 md:grid-cols-4 md:gap-5 w-full max-w-8xl ">
+    <div className={`grid grid-cols-2 md:grid-cols-4 md:gap-5 w-full max-w-8xl ${className || ""}`}>
       <img className="rounded-tl-lg lg:rounded-lg" src="/a.GIF" alt="a" />
       <img className="rounded-tr-lg lg:rounded-lg" src="/b.GIF" alt="b" />
       <img className="rounded-bl-lg lg:rounded-lg" src="/c.GIF" alt="c" />
